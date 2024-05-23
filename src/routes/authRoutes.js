9ae6b51a8f5a44319db6e6e2b9428985e170edc1
tienluandev <nguyentienluan3@gmail.com>
2024-05-23 10:14:58 +0700

    testing login signup

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { login, signup } from "../controllers/authController.js";
 
 const authRoutes = express.Router();
 
+//login
 authRoutes.get("/login", login);
+
+//signup
+authRoutes.get("/signup", signup);
 
 export default authRoutes;
 
